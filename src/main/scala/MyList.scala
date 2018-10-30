15a47b3e43b401fc10066835f1a1ea0a2b65d373
marcyyy0612 <marcyyy0612@gmail.com>
2018-10-31 08:15:07 +0900

    imple trait mylist

--- src/main/scala/MyList.scala
+++ src/main/scala/MyList.scala
@@ -3,10 +3,12 @@ trait MyList[+A] {
   def tail: MyList[A]
 }
 
-case class MyCons[A](
-  ???
+case class MyCons[A] (
+  def head: A = ???
+  def tail: MyList[A] = ???
 ) extends MyList[A]
 
-case object MyNil extends MyList[???] {
-  ???
+case object MyNil extends MyList[Nothing] {
+  def head: Nothing = throw new NoSuchElementException("head of empty list")
+  def tail: MyList[Nothing] = throw new UnsupportedOperationException("tail of empty list")
 }
